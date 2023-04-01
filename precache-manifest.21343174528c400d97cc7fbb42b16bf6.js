5f29356ad73e9a09d1646f3cbac7742168f2f7e2
prathmesh59 <prathmeshpapal453@gmail.com>
2023-04-01 13:01:09 +0530

    Updates

---
+++ precache-manifest.21343174528c400d97cc7fbb42b16bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "306393f9ef9f499afb2d",
+    "url": "/temporary/static/css/main.968dbd5f.chunk.css"
+  },
+  {
+    "revision": "306393f9ef9f499afb2d",
+    "url": "/temporary/static/js/main.306393f9.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/temporary/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/temporary/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "2fd726a3dc1580f01d8d",
+    "url": "/temporary/static/js/runtime~main.2fd726a3.js"
+  },
+  {
+    "revision": "cbc2605b257ebcfb9a0773eec077cfa8",
+    "url": "/temporary/index.html"
+  }
+];
